86d9f8b48d6a152aaf2230253c9e7ad8b2a8da71
novakvova <novakvova@gmail.com>
2022-01-26 15:47:24 +0200

    Add swagger begin

--- src/main/java/com/example/shop/web/HomeController.java
+++ src/main/java/com/example/shop/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo.web;
+package com.example.shop.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
