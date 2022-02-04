401000d2439b6e303a271ce64bbe540b01986fb4
novakvova <novakvova@gmail.com>
2022-02-04 20:41:51 +0200

    Add auth begin

--- my-shop/src/components/Home/index.tsx
+++ my-shop/src/components/Home/index.tsx
@@ -6,6 +6,7 @@ import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import "cropperjs/dist/cropper.min.css";
 import Cropper from "cropperjs";
+import Grid from '@mui/material/Grid';
 
 const HomePage: React.FC = () => {
   // React.useEffect(() => {
@@ -24,23 +25,35 @@ const HomePage: React.FC = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 600,
+    width: "50%",
+    minWidth: "400px",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
   };
   const imgRef = React.useRef<HTMLImageElement>(null);
+  const prevRef = React.useRef<HTMLImageElement>(null);
+  const [cropperObj, setCropperObj] = React.useState<Cropper>();
 
   const handleModal = async () => {
     //console.log("нажали");
     await setOpen(true);
-    const cropper = new Cropper(imgRef.current as HTMLImageElement, {});
-  }
+    const cropper = new Cropper(imgRef.current as HTMLImageElement, {
+      aspectRatio: 16 / 9,
+      viewMode: 1,
+      preview: prevRef.current as HTMLImageElement,
+    });
+    await setCropperObj(cropper);
+  };
 
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleCropped = () => {
+      console.log("image", cropperObj?.getCroppedCanvas().toDataURL());
+      handleClose();
+  }
 
   return (
     <>
@@ -59,16 +72,34 @@ const HomePage: React.FC = () => {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Text in a modal
           </Typography>
-          {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography> */}
-          <div>
-            <img
-              width="100%"
-              ref={imgRef}
-              src="https://vovalohika.tk/images/600_ixdj2v4m.wck.jpeg"
-            />
-          </div>
+          <Grid container spacing={2}>
+            <Grid item md={8}>
+              <div>
+                <img
+                  width="100%"
+                  ref={imgRef}
+                  src="https://vovalohika.tk/images/600_ixdj2v4m.wck.jpeg"
+                />
+              </div>
+            </Grid>
+            <Grid item md={4}>
+              <div className="mb-2"
+                ref={prevRef}
+                style={{
+                  height: "100px",
+                  border: "1px solid silver",
+                  overflow: "hidden",
+                }}
+              >
+                {" "}
+              </div>
+              <div>
+                <Button variant="contained" onClick={handleCropped}>
+                  Отримати фото
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
         </Box>
       </Modal>
     </>
